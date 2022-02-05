77804a676c018f45eed97d98f693d2f1e3076813
Orges Mihaj <orges.mihaj@hotmail.com>
2022-02-05 16:30:45 +0100

    Enhancing Airbnb's ESLint config with TypeScript support;

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,14 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
+    'airbnb-typescript/base',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 13,
     sourceType: 'module',
+    project: './tsconfig.json',
   },
   plugins: [
     '@typescript-eslint',
