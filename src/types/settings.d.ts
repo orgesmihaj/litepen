3aeffb8f9387c0af174e8608e8b3f8e32d5a6b8e
Orges Mihaj <orges.mihaj@hotmail.com>
2022-04-18 01:56:31 +0200

    Describing the properties of the Settings type;

--- src/types/settings.d.ts
+++ src/types/settings.d.ts
@@ -1,10 +1,51 @@
 type Settings = {
+	/**
+	 * Editor Autofocus Mode
+	 * -> Set a Caret to the Editor after initialization.
+	 */
+
 	autofocus?: boolean;
+
+	/**
+	 * Editor Content
+	 * -> Editor's content as an object or DOM element.
+	 */
+
 	content?: Object | HTMLElement;
+
+	/**
+	 * Editor Debounce
+	 * -> The time needed by the editor to wait for the user to stop typing.
+	 */
+
 	debounce?: number;
+
+	/**
+	 * Editor Editable Mode
+	 * -> Enable or disable the editor.
+	 */
+
 	editable?: boolean;
+
+	/**
+	 * Editor Extensions
+	 * -> Activate new functionalities or behaviors to the Editor.
+	 */
+
 	extensions?: Array<any>;
-	holder?: HTMLElement | null;
+
+	/**
+	 * Editor Holder
+	 * -> Bind the Editor to a specific element or selector.
+	 */
+
+	holder: HTMLElement | null;
+
+	/*
+	 * Editor Placeholder
+	 * -> Set a placeholder to the Editor.
+	 */
+
 	placeholder?: string;
 };
 
