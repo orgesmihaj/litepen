97bcb9a1fe16cb553bf43576549e93b7a56e77f4
Orges Mihaj <orges.mihaj@hotmail.com>
2022-05-01 23:59:02 +0200

    Updating tests to consider holder as a required setting;

--- tests/settings.test.ts
+++ tests/settings.test.ts
@@ -3,24 +3,27 @@ import Settings from '../src/settings/settings';
 import TSettings from '../src/types/settings';
 
 describe('Settings object', () => {
+	document.body.innerHTML = `<div class="composer"></div>`;
+	const holder = document.querySelector('.composer');
+
 	const defaults: TSettings = {
 		autofocus: true,
 		content: {},
 		debounce: 330,
 		editable: true,
 		extensions: [],
-		holder: null,
 		placeholder: '',
+		holder,
 	};
 
 	it('can retrieve a list of all of the default settings.', () => {
-		const settings = new Settings();
+		const settings = new Settings({ holder });
 
 		expect(settings.all()).toMatchObject(defaults);
 	});
 
 	it('can retrieve each of the default settings.', () => {
-		const settings = new Settings();
+		const settings = new Settings({ holder });
 
 		expect(settings.get('autofocus')).toBe(defaults.autofocus);
 		expect(settings.get('extensions')).toEqual(defaults.extensions);
@@ -32,7 +35,7 @@ describe('Settings object', () => {
 	});
 
 	it('will change the value of an existing setting.', () => {
-		const settings = new Settings();
+		const settings = new Settings({ holder });
 
 		settings.set('autofocus', false);
 
@@ -40,7 +43,7 @@ describe('Settings object', () => {
 	});
 
 	it('will not update a setting if the new value is undefined.', () => {
-		const settings = new Settings();
+		const settings = new Settings({ holder });
 		const existingValue = settings.get('autofocus');
 
 		settings.set('autofocus', undefined);
@@ -49,12 +52,13 @@ describe('Settings object', () => {
 	});
 
 	it('will override the pre-defined settings in bulk and return them.', () => {
-		const settings = new Settings();
+		const settings = new Settings({ holder });
 
 		const alternations: TSettings = {
 			autofocus: false,
 			debounce: 100,
 			editable: false,
+			holder,
 		};
 
 		expect(settings.overriddenBy(alternations)).toMatchObject({
@@ -68,6 +72,7 @@ describe('Settings object', () => {
 			autofocus: false,
 			debounce: 100,
 			editable: false,
+			holder,
 		};
 
 		const settings = new Settings(alternations);
