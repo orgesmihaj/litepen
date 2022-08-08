17b2ebc8ef28b65c91904a3b5ac8a61bbc83f3dd
Orges Mihaj <OrgesMihaj@users.noreply.github.com>
2022-08-09 00:51:36 +0200

    Small adjustments to Settings;

---
+++ src/contracts/settings.ts
@@ -0,0 +1,37 @@
+import TSettings from '../types/settings';
+
+interface ISettings {
+	/**
+	 * Get a list of all of the configuration items.
+	 *
+	 * @return {TSettings}
+	 */
+	all(): TSettings;
+
+	/**
+	 * Get a the value of a specific setting.
+	 *
+	 * @param  {Key} key
+	 * @returns TSettings
+	 */
+	get<Key extends keyof TSettings>(key: Key): TSettings[Key];
+
+	/**
+	 * Override the default settings by the given ones.
+	 *
+	 * @param  {TSettings} settings
+	 * @returns TSettings
+	 */
+	overriddenBy(settings: TSettings): TSettings;
+
+	/**
+	 * Set a given configuration value.
+	 *
+	 * @param  {Key} key
+	 * @param  {TSettings[Key]} value
+	 * @returns void
+	 */
+	set<Key extends keyof TSettings>(key: Key, value: TSettings[Key]): void;
+}
+
+export default ISettings;
