2c020c0f0b9fdcbb4af855d3c1b9ddfd41a367d1
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2023-01-05 23:46:22 -0500

    Moving the `types` dir outside the `src` dir;

--- types/settings.d.ts
+++ types/settings.d.ts
@@ -1,51 +1,51 @@
 type TSettings = {
 	/**
 	 * Editor Autofocus Mode
-	 * -> Set a Caret to the Editor after initialization.
+	 *
+	 * Set a Caret to the Editor after initialization.
 	 */
-
 	autofocus?: boolean;
 
 	/**
 	 * Editor Content
-	 * -> Editor's content as an object or DOM element.
+	 *
+	 * Editor's content as an object or DOM element.
 	 */
-
 	content?: Object | HTMLElement;
 
 	/**
 	 * Editor Debounce
-	 * -> The time needed by the editor to wait for the user to stop typing.
+	 *
+	 * The time needed by the editor to wait for the user to stop typing.
 	 */
-
 	debounce?: number;
 
 	/**
 	 * Editor Editable Mode
-	 * -> Enable or disable the editor.
+	 *
+	 * Enable or disable the editor.
 	 */
-
 	editable?: boolean;
 
 	/**
 	 * Editor Extensions
-	 * -> Activate new functionalities or behaviors to the Editor.
+	 *
+	 * Activate new functionalities or behaviors to the Editor.
 	 */
-
 	extensions?: Array<any>;
 
 	/**
 	 * Editor Holder
-	 * -> Bind the Editor to a specific element or selector.
+	 *
+	 * Bind the Editor to a specific HTML element or selector.
 	 */
-
 	holder: Element | null;
 
 	/*
 	 * Editor Placeholder
-	 * -> Set a placeholder to the Editor.
+	 *
+	 * Set a placeholder to the Editor.
 	 */
-
 	placeholder?: string;
 };
 
