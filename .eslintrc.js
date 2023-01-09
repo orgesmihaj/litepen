95324eda3732a6f8075c06ed2c8e08181f6ab8b9
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2023-01-09 18:52:03 -0500

    ESLint updates:
    - Fixing indentations
    - Disable `spaced-comment`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,19 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'airbnb-base',
-    'airbnb-typescript/base',
-    'prettier',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 13,
-    sourceType: 'module',
-    project: './tsconfig.json',
-  },
-  plugins: [
-    '@typescript-eslint',
-  ],
-  rules: {
-    "class-methods-use-this": "off",
-    "no-new": "off",
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['airbnb-base', 'airbnb-typescript/base', 'prettier'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 13,
+		sourceType: 'module',
+		project: './tsconfig.json',
+	},
+	plugins: ['@typescript-eslint'],
+	rules: {
+		'class-methods-use-this': 'off',
+		'no-new': 'off',
+		'spaced-comment': 0,
+	},
 };
