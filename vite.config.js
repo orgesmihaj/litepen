935b2f57b52966e2fe94595cf6ff5fe3dbcf4409
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2023-01-10 20:41:38 -0500

    Setting up the path aliases;

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,17 @@
+import path from 'node:path';
 
 export default {
-  test: { environment: 'jsdom' },
-}
+	test: { environment: 'jsdom' },
+
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@assets': path.resolve(__dirname, './src/assets'),
+			'@contacts': path.resolve(__dirname, './contacts'),
+			'@factories': path.resolve(__dirname, './factories'),
+			'@src': path.resolve(__dirname, './src'),
+			'@types': path.resolve(__dirname, './types'),
+			'@ui': path.resolve(__dirname, './src/ui'),
+		},
+	},
+};
