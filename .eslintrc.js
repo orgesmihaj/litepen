16fa666cffd375517c5da78725a0d211638b1224
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2023-01-10 20:45:52 -0500

    Set ESLint to ignore the file extension in imports;

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,16 @@ module.exports = {
 		'class-methods-use-this': 'off',
 		'no-new': 'off',
 		'spaced-comment': 0,
+		'import/extensions': [
+			'error',
+			'ignorePackages',
+			{
+				'': 'never',
+				js: 'never',
+				jsx: 'never',
+				ts: 'never',
+				tsx: 'never',
+			},
+		],
 	},
 };
