a06fb68ebe3e96a57462b4cf7950e78d0abb33ca
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2023-01-10 21:09:04 -0500

    Removing the redundant `@param` and `@return`;

--- contracts/editor.ts
+++ contracts/editor.ts
@@ -1,23 +1,16 @@
 interface IEditor {
 	/**
 	 * Detect any change in the editor content.
-	 *
-	 * @returns void
 	 */
 	onChange(): void;
 
 	/**
 	 * Convert the content to JSON or HTML.
-	 *
-	 * @param  {Boolean} asHTML
-	 * @returns void
 	 */
 	onSave(asHTML: boolean): void;
 
 	/**
 	 * Bootstrap the editor.
-	 *
-	 * @returns Element
 	 */
 	ignite(): Element;
 }
