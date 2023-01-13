e972fd2d4177fbf9745d79268f41eebe13ef3e34
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2023-01-12 19:32:59 -0500

    Creating the `workshop` directory to store different implementations of the editor;

--- src/composer.ts
+++ src/composer.ts
@@ -1,28 +1,13 @@
 import TSettings from 'types/settings';
-import Settings from './settings';
-import EditorUI from './ui/editorUI';
-import Mutation from './mutation';
-import Editor from './editor';
-
-import './assets/sass/objects/editor.scss';
+import FEditor from '@factories/editor';
+import Settings from '@/settings';
 
 class Composer {
 	constructor(settings: TSettings) {
-		const editor = new Editor(
-			new EditorUI(new Settings(settings)),
-			new Mutation()
-		);
+		Settings.use(settings);
 
-		editor.ignite();
+		new FEditor().assemble().ignite();
 	}
 }
-
-/**
- * OUTPUT
- */
-new Composer({
-	holder: document.querySelector('.composer'),
-	placeholder: 'Write something cool...',
-});
 
 export default Composer;
