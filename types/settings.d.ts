c530174e7616b8f7a7c7e90ec606a743ca7a409e
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2023-02-07 17:40:10 -0500

    Updating comments;

--- types/settings.d.ts
+++ types/settings.d.ts
@@ -1,49 +1,35 @@
 export type TSettings = {
 	/**
-	 * Editor Autofocus Mode
-	 *
 	 * Set a Caret to the Editor after initialization.
 	 */
 	autofocus?: boolean;
 
 	/**
-	 * Editor Content
-	 *
 	 * Editor's content as an object or DOM element.
 	 */
 	content?: Object | HTMLElement;
 
 	/**
-	 * Editor Debounce
-	 *
 	 * The time needed by the editor to wait for the user to stop typing.
 	 */
 	debounce?: number;
 
 	/**
-	 * Editor Editable Mode
-	 *
 	 * Enable or disable the editor.
 	 */
 	editable?: boolean;
 
 	/**
-	 * Editor Extensions
-	 *
 	 * Activate new functionalities or behaviors to the Editor.
 	 */
 	extensions?: Array<any>;
 
 	/**
-	 * Editor Holder
-	 *
 	 * Bind the Editor to a specific HTML element or selector.
 	 */
 	holder: Element;
 
 	/*
-	 * Editor Placeholder
-	 *
 	 * Set a placeholder to the Editor.
 	 */
 	placeholder?: string;
