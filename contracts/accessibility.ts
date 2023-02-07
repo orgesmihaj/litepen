07588a0175b5a4613a81afe0cab64a8811730cec
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2023-02-06 21:41:18 -0500

    Adding the `hasSemanticMeaning` method to the Accessibility contract;

--- contracts/accessibility.ts
+++ contracts/accessibility.ts
@@ -5,8 +5,13 @@ interface IAccessibility {
 	/**
 	 * Change the paragraph separator tag used when new
 	 * paragraphs are created.
-	 * */
+	 */
 	paragraphSeparator: (tag: HTMLElement) => void;
+
+	/**
+	 * Check if the element has semantic meaning.
+	 */
+	hasSemanticMeaning: (tag: HTMLElement) => boolean;
 }
 
 export default IAccessibility;
