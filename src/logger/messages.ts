1ddbc9d66f6ba54da9b6635fe702b54967c1c485
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2023-02-07 17:44:39 -0500

    Adding the error message for cases when the HTML element has no semantic meaning;

--- src/logger/messages.ts
+++ src/logger/messages.ts
@@ -1,5 +1,6 @@
 enum Messages {
 	HOLDER_IS_MISSING = 'The holder is missing. Please, checkout whether a DOM Element is properly selected.',
+	NO_SEMANTIC_MEANING = 'The element does not have semantic meaning.',
 }
 
 export default Messages;
