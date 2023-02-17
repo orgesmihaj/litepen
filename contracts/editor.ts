8ce682d8343fa07e6853a2d6ead06d5865c79ce7
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2023-02-17 14:40:59 -0500

    Removing `onChange` and `onSave` from Editor's contract;

--- contracts/editor.ts
+++ contracts/editor.ts
@@ -1,15 +1,5 @@
 interface IEditor {
 	/**
-	 * Detect any change in the editor content.
-	 */
-	onChange(): void;
-
-	/**
-	 * Convert the content to JSON or HTML.
-	 */
-	onSave(records: MutationRecord[]): void;
-
-	/**
 	 * Bootstrap the editor.
 	 */
 	ignite(): void;
