840026aba85c1c1fa5d5209f867d43be96c3617f
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2023-03-18 14:07:10 -0400

    Defining the `Designer` interface.

---
+++ contracts/ui/designer.ts
@@ -0,0 +1,36 @@
+/**
+ * Modify the DOM of an element.
+ */
+interface IDesigner {
+	/**
+	 * Make the element editable or not.
+	 */
+	editable(status: boolean | undefined): this;
+
+	/**
+	 * Check if the element has a placeholder.
+	 */
+	hasPlaceholder(): boolean;
+
+	/**
+	 * Attach a `data-*` attribute to the element.
+	 */
+	identifyAs(name: string): this;
+
+	/**
+	 * Check if the element is editable.
+	 */
+	isEditable(): boolean;
+
+	/**
+	 * Assign the element to the designer.
+	 */
+	on(element: Element): this;
+
+	/**
+	 * Set the placeholder for the element.
+	 */
+	placeholder(message?: string | false | undefined): this;
+}
+
+export default IDesigner;
