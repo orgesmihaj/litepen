a5a8e0d4fb68ac6ea318553f10f953564170decd
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2023-04-06 18:12:00 -0400

    Adding `NO_ELEMENT_TO_OBSERVE` logging message.

--- src/logger/messages.ts
+++ src/logger/messages.ts
@@ -1,5 +1,6 @@
 enum Messages {
 	HOLDER_IS_MISSING = 'The holder is missing. Please, checkout whether a DOM Element is properly selected.',
+	NO_ELEMENT_TO_OBSERVE = 'No element to observe.',
 	NO_SEMANTIC_MEANING = 'The element does not have semantic meaning.',
 }
 
