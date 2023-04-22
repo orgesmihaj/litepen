d5c4ca3bbeac1cc1ad1c78a967b90723f0a1726d
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2023-04-22 13:11:13 -0400

    Fixing formatting and ordering issues.

--- src/ui/keyboard.ts
+++ src/ui/keyboard.ts
@@ -1,30 +1,31 @@
-import IKeyboard from '@contracts/ui/keyboard';
-import IListeners from '@contracts/listeners';
-import { TListenerCallback } from 'types/listeners';
+import IKeyboard from "@contracts/ui/keyboard";
+import IListeners from "@contracts/listeners";
+import { TBlueprint } from "types/designer/keyboard";
+import { TListenerCallback } from "types/listeners";
 
 /**
  * Handle keyboard-related events.
  * */
 class Keyboard implements IKeyboard {
-	/**
-	 * A map of listeners attached to the editor.
-	 * */
-	private listeners: IListeners;
+  /**
+   * A map of listeners attached to the editor.
+   * */
+  private listeners: IListeners;
 
-	constructor(listeners: IListeners) {
-		this.listeners = listeners;
-	}
+  constructor(blueprint: TBlueprint) {
+	this.listeners = blueprint.listeners;
+  }
 
-	/**
-	 * Run a callback when the defined key is pressed.
-	 * */
-	whenPressing(key: string, callback: TListenerCallback): void {
-		this.listeners.subscribe('keydown', event => {
-			if ((event as KeyboardEvent).key === key) {
-				callback(event);
-			}
-		});
-	}
+  /**
+   * Run a callback when the defined key is pressed.
+   * */
+  whenPressing(key: string, callback: TListenerCallback): void {
+	this.listeners.subscribe('keydown', event => {
+	  if ((event as KeyboardEvent).key === key) {
+		callback(event);
+	  }
+	});
+  }
 }
 
 export default Keyboard;
