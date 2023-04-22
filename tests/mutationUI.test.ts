03f09741eaaa023b158946a4585eb3e1722a8f81
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2023-04-22 13:05:07 -0400

    Deleting `mutationUI.test.ts`.

--- tests/mutationUI.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest';
-
-describe('DOM mutation mechanism.', () => {
-	it('will initialize the DOM mutation mechanism.', () => {
-		// const mutation = new Observer().whenMutations(() => {});
-		expect('').toBeDefined();
-	});
-});
