c9f85c5472b577e065185af2385d7c892138e2af
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2023-04-22 13:08:32 -0400

    Fixing formatting issues.

--- contracts/ui/keyboard.ts
+++ contracts/ui/keyboard.ts
@@ -1,12 +1,12 @@
-import { TListenerCallback } from 'types/listeners';
+import { TListenerCallback } from "types/listeners";
 
 /**
  * Handle keyboard-related events.
- * */
+ */
 interface IKeyboard {
 	/**
 	 * Run a callback when the defined key is pressed.
-	 * */
+	 */
 	whenPressing(key: string, callback: TListenerCallback): void;
 }
 
