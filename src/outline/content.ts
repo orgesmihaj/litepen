2dc8f6e89719798ab293002f49e11cea590ff815
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2023-04-22 13:04:40 -0400

    Fixing formatting issues.

--- src/outline/content.ts
+++ src/outline/content.ts
@@ -1,51 +1,51 @@
-import IContent from "@contracts/outline/content";
-import { TContentCatalogue } from "types/catalogue";
-import HContent from "@/helper/content";
+import IContent from '@contracts/outline/content';
+import { TContentCatalogue } from 'types/catalogue';
+import HContent from '@/helper/content';
 
 /**
  * Operations that are common to both simple and complex
  * elements of the editor's content.
  */
 abstract class Content implements IContent {
-	/**
-	 * The content written in the editor.
-	 */
-	protected abstract content: unknown;
+  /**
+   * The content written in the editor.
+   */
+  protected abstract content: unknown;
 
-	/**
-	 * Unique identifier of the content.
-	 */
-	readonly id: string = HContent.uniqueIdentifier();
+  /**
+   * Unique identifier of the content.
+   */
+  readonly id: string = HContent.uniqueIdentifier();
 
-	/**
-	 * A composite of one or more pieces of content.
-	 */
-	protected structure: IContent[] = [];
+  /**
+   * A composite of one or more pieces of content.
+   */
+  protected structure: IContent[] = [];
 
-	/**
-	 * Type of the content as defined in the
-	 * content catalogue.
-	 */
-	readonly type: keyof TContentCatalogue = 'paragraph';
+  /**
+   * Type of the content as defined in the
+   * content catalogue.
+   */
+  readonly type: keyof TContentCatalogue = 'paragraph';
 
-	/*
-	 * Retrieve the DOM element that represents
-	 * the content.
-	 */
-	abstract element(): HTMLElement;
+  /*
+   * Retrieve the DOM element that represents
+   * the content.
+   */
+  abstract element(): HTMLElement;
 
-	/**
-	 * Determine whether the content is a composite
-	 * of one or more pieces of content.
-	 */
-	isComposite(): boolean {
-		return false;
-	}
+  /**
+   * Determine whether the content is a composite
+   * of one or more pieces of content.
+   */
+  isComposite(): boolean {
+	return false;
+  }
 
-	/**
-	 * Update the content.
-	 */
-	abstract update(mutations: MutationRecord[]): void;
+  /**
+   * Update the content.
+   */
+  abstract update(mutations: MutationRecord[]): void;
 }
 
 export default Content;
