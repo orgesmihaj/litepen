efa697b4a950f41da01b0827ab9fbf51e8fcadbf
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2023-04-24 20:31:28 -0400

    Fixing formatting issues.

--- contracts/outline/content.ts
+++ contracts/outline/content.ts
@@ -5,51 +5,51 @@ import { TContentCatalogue } from 'types/catalogue';
  * and complex elements of the editor's content.
  */
 interface IContent {
-  /**
-   * Unique identifier of the content.
-   */
-  readonly id: string;
+	/**
+	 * Unique identifier of the content.
+	 */
+	readonly id: string;
 
-  /**
-   * Type of the content as defined in the
-   * content catalogue.
-   */
-  readonly type: keyof TContentCatalogue;
+	/**
+	 * Type of the content as defined in the
+	 * content catalogue.
+	 */
+	readonly type: keyof TContentCatalogue;
 
-  /**
-   * Add a piece of content to the editor's state.
-   */
-  add?(content: IContent): void;
+	/**
+	 * Add a piece of content to the editor's state.
+	 */
+	add?(content: IContent): void;
 
-  /**
-   * Define the content's DOM element.
-   */
-  element(): HTMLElement;
+	/**
+	 * Define the content's DOM element.
+	 */
+	element(): HTMLElement;
 
-  /**
-   * Erase a piece of content from the editor's state.
-   */
-  erase?(content: IContent): void;
+	/**
+	 * Erase a piece of content from the editor's state.
+	 */
+	erase?(content: IContent): void;
 
-  /**
-   * Determine whether the content is a composite
-   * of one or more pieces of content.
-   */
-  isComposite(): boolean;
+	/**
+	 * Determine whether the content is a composite
+	 * of one or more pieces of content.
+	 */
+	isComposite(): boolean;
 
-  /**
-   * Add a trailing element to the content.
-   *
-   * @note The trailing element is necessary because without it, an empty
-   * 			 contenteditable element may not have a visual representation,
-   * 			 leading to difficulties in formatting and editing.
-   */
-  trailingElement?(): HTMLElement;
+	/**
+	 * Add a trailing element to the content.
+	 *
+	 * @note The trailing element is necessary because without it, an empty
+	 * 			 contenteditable element may not have a visual representation,
+	 * 			 leading to difficulties in formatting and editing.
+	 */
+	trailingElement?(): HTMLElement;
 
-  /**
-   * Update the content.
-   */
-  update(mutations: MutationRecord[]): void;
+	/**
+	 * Update the content.
+	 */
+	update(mutations: MutationRecord[]): void;
 }
 
 export default IContent;
