2d5bdba453397359b40149d61b8d94a4684debaa
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2023-04-26 15:39:01 -0400

    Content-related types
    - Defining type structures to accommodate various content types.
    - Defining the structure of the blueprint that can be used to assemble a new piece of content.
    - Defining the structure of a content constructor.

--- types/content.d.ts
+++ types/content.d.ts
@@ -1,6 +1,31 @@
+import IContent from '@contracts/outline/content';
+
 /**
  * The structure of a paragraph.
  */
 export type TParagraph = {
 	text: string;
+};
+
+/**
+ * The structure of an empty content.
+ */
+export type TEmptyContent = {};
+
+/**
+ * The structure of a content.
+ */
+export type TContent = TParagraph | TEmptyContent;
+
+/**
+ * The structure of a content constructor.
+ */
+export type TContentConstructor = new (blueprint?: TBlueprint) => IContent;
+
+/**
+ * The structure of the blueprint that can be used
+ * to assemble a new piece of content.
+ */
+export type TBlueprint = {
+	content?: TContent;
 };
