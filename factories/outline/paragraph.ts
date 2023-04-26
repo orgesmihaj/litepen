9df2ee1ea39a54749b5baeacd6bc2f55a7c04efa
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2023-04-26 15:51:21 -0400

    Passing the blueprint argument to dictate the assembly process for the paragraph instance.

--- factories/outline/paragraph.ts
+++ factories/outline/paragraph.ts
@@ -1,6 +1,7 @@
-import IContent from "@contracts/outline/content";
-import IFactory from "@contracts/factory";
-import Paragraph from "@/outline/paragraph";
+import IContent from '@contracts/outline/content';
+import IFactory from '@contracts/factory';
+import { TBlueprint } from 'types/content';
+import Paragraph from '@/outline/paragraph';
 
 /**
  * Define a paragraph as part of the editor's
@@ -10,8 +11,8 @@ class FParagraph implements IFactory<IContent> {
 	/**
 	 * Assemble a new `Paragraph` instance.
 	 */
-	assemble(): IContent {
-		return new Paragraph();
+	assemble(blueprint?: TBlueprint): IContent {
+		return new Paragraph(blueprint);
 	}
 }
 
