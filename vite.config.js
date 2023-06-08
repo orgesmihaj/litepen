9eb772548bd12565b121084e3f6a9e549215d1a9
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2023-06-08 19:48:57 -0400

    Preparing Vite configs for monorepo.

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,39 @@
 import path from 'node:path';
 
-export default {
-	test: { environment: 'jsdom' },
+/**
+ * 🤝Aliases for importing modules.
+ */
+const ALIASES = {
+	'@': path.resolve(__dirname, './src'),
+	'@assets': path.resolve(__dirname, './src/assets'),
+	'@contacts': path.resolve(__dirname, './contacts'),
+	'@factories': path.resolve(__dirname, './factories'),
+	'@logger': path.resolve(__dirname, './src/logger'),
+	'@src': path.resolve(__dirname, './src'),
+	'@types': path.resolve(__dirname, './types'),
+}
 
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-			'@assets': path.resolve(__dirname, './src/assets'),
-			'@contacts': path.resolve(__dirname, './contacts'),
-			'@factories': path.resolve(__dirname, './factories'),
-			'@logger': path.resolve(__dirname, './src/logger'),
-			'@src': path.resolve(__dirname, './src'),
-			'@types': path.resolve(__dirname, './types'),
-			'@ui': path.resolve(__dirname, './src/ui'),
-		},
-	},
+/**
+ * 🧱The configuration for building a library.
+ */
+const LIBRARY_BUILD_CONFIG = {
+	cssCodeSplit: false,
+	entry: path.resolve(__dirname, 'src/composer.ts'),
+	fileName: 'composer.js',
+	formats: ['es', 'umd', 'cjs', 'iife'],
+	name: 'composer',
+	sourcemap: false,
+};
+
+/**
+ * 🔎The configuration for testing.
+ */
+const TEST_CONFIG = {
+	environment: 'jsdom',
+};
+
+export default {
+	build: { lib: LIBRARY_BUILD_CONFIG },
+	resolve: { alias: ALIASES },
+	test: TEST_CONFIG,
 };
