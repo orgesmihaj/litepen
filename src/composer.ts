7f1f891bd7e53b107e687a71e62d99488d38d406
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2023-07-04 00:29:00 -0400

    Implementing the `IComposer` contract and adjusting the code.

--- src/composer.ts
+++ src/composer.ts
@@ -1,9 +1,20 @@
-import { TSettings } from 'types/settings';
+import IComposer from '@contracts/composer';
 import FEditor from '@factories/editor';
+import { TSettings } from 'types/settings';
 import Settings from '@/settings';
 
-class Composer {
+/**
+ * What's on your mind? ...✏️
+ */
+class Composer implements IComposer {
 	constructor(settings: TSettings) {
+		this.build(settings);
+	}
+
+	/**
+	 * Build the composer.
+	 */
+	build(settings: TSettings): void {
 		Settings.use(settings);
 
 		new FEditor().assemble().ignite();
