2d178e805e120bf66f0bfcb04c999ac73e90ead0
Orges Mihaj <10802548+orgesmihaj@users.noreply.github.com>
2024-12-08 21:29:58 -0500

    Cleaning up. 🧹

--- packages/workshop/.eslintrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	root: true,
-	extends: ["@litepen/eslint-config-litepen"],
-};
--- packages/workshop/src/main.ts
+++
@@ -1,9 +0,0 @@
-import Composer from '@litepen/core';
-
-import './sass/semantic.scss';
-import './sass/composer.scss';
-
-new Composer({
-	holder: document.getElementById('playground__composer'),
-	placeholder: 'Write something...',
-});
